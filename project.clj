feb9b9277beddd1d35ddf59f7c7fafc36ae70bb3
Jon Witte <jon.witte@bevuta.com>
2024-02-15 18:02:52 +0100

    upgrade clj + cljs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kovacnica/clojure.network.ip "0.1.3"
+(defproject kovacnica/clojure.network.ip "0.1.4"
   :description "Clojure/Clojurescript library for IP address and Network operations."
   :target-path "target/%s/"
   :compile-path "%s/classy-files"
@@ -6,8 +6,8 @@
   :source-paths ["src"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.132"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :cljsbuild {:builds [{:id "network"
